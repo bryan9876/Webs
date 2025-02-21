5bff478cb63a442ee8f1c3124a0f4086fac63cd4
Bryan Alejandro Martinez Lopez <91569580+bryan9876@users.noreply.github.com>
2025-02-20 22:16:49 -0600

    commit

---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports.users = [
+    {
+        id:1,
+        name:"John",
+        email:"john@gmail.com",
+        password:"123456"
+    },
+    {
+        id:2,
+        name:"Jane",
+        email:"jane@gmail.com",
+        password:"1234567"
+    }
+];
