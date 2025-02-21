96e637c5a0c4f3e9ea9bb4f4d5016d92fe7c7509
Bryan Alejandro Martinez Lopez <91569580+bryan9876@users.noreply.github.com>
2025-02-20 22:15:34 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const mensaje = document.getElementById("mensaje");
-const boton = document.getElementById("boton-saludar");
-
-const evento = () => {
-    let textomensaje = "Hola desde el JavaScript ";
-    mensaje.innerHTML = textomensaje;
-};
-
-boton.addEventListener("click", evento);
